7a30ce4ce102048732f568a8d13e1901e18789c9
Danyil Marchuk <zemero58@gmail.com>
2023-07-03 17:45:42 +0200

    commit

--- InterestingNumbers/FactsScreen.swift
+++ InterestingNumbers/FactsScreen.swift
@@ -19,6 +19,9 @@ class YourViewController: UIViewController, UICollectionViewDelegate, UICollecti
     ]
 
     override func viewDidLoad() {
+        
+        
+        
         super.viewDidLoad()
         view.backgroundColor = .purple
 
